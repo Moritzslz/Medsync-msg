d2ee06116d3ad2343010b4d0623ebc1e6e164218
climbingncode <55851811+climbingncode@users.noreply.github.com>
2024-06-22 00:35:45 +0200

    Initial DoctorController and PatientController

--- src/main/java/msg/medsync/Controller/DoctorController.java
+++ src/main/java/msg/medsync/Controller/DoctorController.java
@@ -1,17 +1,27 @@
 package msg.medsync.Controller;
 
 import lombok.NoArgsConstructor;
+import msg.medsync.Models.Doctor;
+import msg.medsync.Repositories.DoctorRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1/doctors/")
 public class DoctorController {
 
-    public DoctorController() {
-    }
+    @Autowired
+    public final DoctorRepository doctorRepository;
 
     @PostMapping("/create")
-    public void createPatient() {
-        // TODO
+    public HttpStatus createDoctor(@RequestBody Doctor doctor) {
+        doctorRepository.save(doctor);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    GetById(@PathVariable Long id) {
+        return doctorRepository.findById(id).get();
     }
 }
--- src/main/java/msg/medsync/Controller/PatientController.java
+++ src/main/java/msg/medsync/Controller/PatientController.java
@@ -1,17 +1,28 @@
 package msg.medsync.Controller;
 
 import lombok.NoArgsConstructor;
+import msg.medsync.Models.Patient;
+import msg.medsync.Repositories.PatientRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1/patients")
 public class PatientController {
-
-    public PatientController() {
-    }
+    
+    @Autowired
+    public final PatientRepository patientRepository;
 
     @PostMapping("/create")
-    public void createPatient() {
-        // TODO
+    public HttpStatus createPatient(@RequestBody Patient patient) {
+        patientRepository.save(patient);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
+
+    @GetMapping("/{id}")
+    GetById(@PathVariable Long id) {
+        return patientRepository.findById(id).get();
+    }
+    
 }
