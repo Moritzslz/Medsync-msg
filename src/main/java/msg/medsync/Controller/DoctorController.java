2f7f777f9b7624b59ebd21533c58fc86396a9609
moritzslz <schultzmoritz@gmail.com>
2024-06-22 11:59:52 +0200

    Doctor Patient Relation

--- src/main/java/msg/medsync/Controller/DoctorController.java
+++ src/main/java/msg/medsync/Controller/DoctorController.java
@@ -1,20 +1,25 @@
 package msg.medsync.Controller;
 
 import msg.medsync.Models.Doctor;
+import msg.medsync.Models.Patient;
 import msg.medsync.Models.PatientDoctor;
 import msg.medsync.Repositories.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/doctor")
 public class DoctorController {
 
     private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
     private final PatientDoctorRepository patientDoctorRepository;
 
-    public DoctorController(DoctorRepository doctorRepository, PatientDoctorRepository patientDoctorRepository) {
+    public DoctorController(DoctorRepository doctorRepository, PatientRepository patientRepository, PatientDoctorRepository patientDoctorRepository) {
         this.doctorRepository = doctorRepository;
+        this.patientRepository = patientRepository;
         this.patientDoctorRepository = patientDoctorRepository;
     }
 
@@ -32,6 +37,24 @@ public class DoctorController {
             return ResponseEntity.notFound().build();
         } else {
             return ResponseEntity.ok().body(patientDoctorIterable);
+        }
+    }
+
+    @PostMapping("{id}/add/patient/{patiendId}")
+    public ResponseEntity<PatientDoctor> addPatient(@PathVariable Long id, @PathVariable Long patientId) {
+        Optional<Patient> patient = patientRepository.findById(patientId);
+        if (patient.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            Patient currentPatient = patient.get();
+            PatientDoctor patientDoctor = new PatientDoctor();
+            patientDoctor.setPatientId(currentPatient.getPatientId());
+            patientDoctor.setDoctorId(id);
+            patientDoctor.setPatientName(currentPatient.getName());
+            patientDoctor.setPatientSurname(currentPatient.getSurname());
+            patientDoctor.setPatientKVR(currentPatient.getKVR());
+            patientDoctorRepository.save(patientDoctor);
+            return ResponseEntity.ok().body(patientDoctor);
         }
     }
 
